d03874d39ee5ca71e251a129a9f1609f810daf3e
vitbelik <vitbelik@mail.ru>
2017-11-08 15:41:08 +0300

    some changes

--- Firs_Steps/src/Hello/newQueue.java
+++ Firs_Steps/src/Hello/newQueue.java
@@ -1,5 +1,50 @@
 package Hello;
 
+class Queue{
+	private char q[]; //this array holds the queue
+	private int putloc, getloc; // the put and get indices
+		
+	Queue (int size){
+		q = new char[size]; //allocate memory for queue
+		putloc = getloc = 0;
+	}
+	
+	Queue(Queue ob){
+		putloc = ob.putloc;
+		getloc = ob.getloc;
+		q = new char[ob.q.length];
+		
+		//copy elements
+		for (int i = getloc; i < putloc; i++)
+			q[i] = ob.q[i];
+	}
+	
+	Queue(char a[]){
+		putloc = 0;
+		getloc = 0;
+		q = new char[a.length];
+		
+		for (int i = 0; i < a.length; i++) put(a[i]);
+	}
+	
+	void put(char ch) {
+		if(putloc == q.length) {
+			System.out.println("- Queue is full.");
+			return;
+		}
+		
+		q[putloc++] = ch;
+	}
+	
+	char get() {
+		if(getloc == putloc) {
+			System.out.println(" - Queue is empty.");
+			return (char) 0;
+		}
+		return q[getloc++];
+	}
+}
+
 class newQueue {
 	public static void main (String args[]) {
 		
@@ -28,7 +73,7 @@ class newQueue {
 		System.out.println("\n");
 		
 		System.out.println("Content of q3: ");
-		for (i = 0; i < 10: i++) {
+		for (i = 0; i < 10; i++) {
 			ch = q3.get();
 			System.out.print(ch);
 		}
@@ -74,48 +119,5 @@ class newQueue {
 		*/
 }
 
-class Queue{
-	private char q[]; //this array holds the queue
-	private int putloc, getloc; // the put and get indices
-		
-	Queue (int size){
-		q = new char[size]; //allocate memory for queue
-		putloc = getloc = 0;
-	}
-	
-	Queue(Queue ob){
-		putloc = ob.putloc;
-		getloc = ob.getloc;
-		q = new char[ob.q.length];
-		
-		//copy elements
-		for (int i = getloc; i < putloc; i++)
-			q[i] = ob.q[i];
-	}
-	
-	Queue(char a[]){
-		putloc = 0;
-		getloc = 0;
-		q = new char[a.length];
-		
-		for (int i = 0; i < a.length; i++) put(a[i]);
-	}
-	
-	void put(char ch) {
-		if(putloc == q.length) {
-			System.out.println("- Queue is full.");
-			return;
-		}
-		
-		q[putloc++] = ch;
-	}
-	
-	char get() {
-		if(getloc == putloc) {
-			System.out.println(" - Queue is empty.");
-			return (char) 0;
-		}
-		return q[getloc++];
-	}
-}
+
 
